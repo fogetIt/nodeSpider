412d1a21d086ec72cb54063eb3617252b4bb90f6
thinkMMP <2271404280@qq.com>
2017-10-26 11:45:24 +0800

    change optionsUtils

--- src/engine/utils/optionsUtils.js
+++ src/engine/utils/optionsUtils.js
@@ -1,33 +1,33 @@
 /*
  * @Date:   2017-08-28 10:53:50
- * @Last Modified time: 2017-10-25 12:34:38
+ * @Last Modified time: 2017-10-26 11:42:00
  */
 'use strict';
 
 
+let headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"
+};
+
+
 export default {
     createOptions: req => {
-        let _url = req.url;
-        if (!req.extData.options || JSON.stringify(req.extData.options) === "{}") {
-            return {
+        let _url = req.url,
+            _options = {
                 url: _url,
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"
-                },
+                headers: headers,
                 resolveWithFullResponse: true,
                 timeout: 60000,
                 encoding: null
             };
-        } else {
-            let _options = {};
+        if (req.extData.options && !JSON.stringify(req.extData.options) === "{}") {
             /*
             deep copy
              */
             for (let key in req.extData.options) {
                 _options[key] = req.extData.options[key];
             }
-            _options.url = _url;
-            return _options;
         }
+        return _options;
     }
 };
